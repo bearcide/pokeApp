e0f22019a03ffdc90f00bac88bddf7f9a9e43f26
bearcide <68358601+bearcide@users.noreply.github.com>
2024-05-07 16:52:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const namePoke2 = document.querySelector('#nombrePoke-rival');
 const poke2Tipo = document.querySelector('#tipoRival');
 const poke2Ataque = document.querySelector('#ataqueRival');
 
+const imgPokeProp = document.querySelector('#pokeProp');
+const namePokeProp = document.querySelector('#nombrePoke-Prop');
+const pokePropTipo = document.querySelector('#tipoProp');
+const pokePropAtaque = document.querySelector('#ataqueProp');
+
+const input = document.querySelector('#input');
+const btnElegir = document.querySelector('#btn-poke');
+const btnCombate = document.querySelector('#btn-combate');
+
 
 const getNumRandom = () => {
     let min = Math.ceil(0);
@@ -12,7 +21,18 @@ const getNumRandom = () => {
   }
 
 const obtenerPokePropio = ()=>{
+    const num = input.value;
+    
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`).then((res)=>{
 
+        return res.data
+    }).then((res)=>{
+        console.log(res);
+        imgPokeProp.src = res.sprites.back_default;
+        pokePropTipo.innerHTML = res.types[0].type.name;
+        namePokeProp.innerHTML = res.name;
+        pokePropAtaque.innerHTML = res.stats[0].base_stat;
+    })
 }
 
 const obtenerPokeRival = () =>{
@@ -29,13 +49,20 @@ const obtenerPokeRival = () =>{
         namePoke2.innerHTML = res.name;
         poke2Ataque.innerHTML = res.stats[0].base_stat;
     })
+
+    console.log(imgPoke2);
 }
 
 const combate = ()=>{
-    
+    const ataqueRival = parseInt(poke2Ataque.textContent);
+    const ataquePropio = pareseInt(pokePropAtaque.textContent);
+
+
 }
 
 window.addEventListener('load', obtenerPokeRival)
+btnElegir.addEventListener('click', obtenerPokePropio);
+btnCombate.addEventListener('click', combate);
 
 
 
